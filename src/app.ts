b74f68aa7b9b46738ebb733f0854885a5caf201d
webnur <mdnuruli291@gmail.com>
2024-04-22 15:23:39 +0600

    Implemented a custom route to handle requests for non-existent routes, responding with a 404 status code and a corresponding error message

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import httpStatus from "http-status";
 
 const app: Application = express();
 
@@ -11,6 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+//handle Not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: {
+      path: req.originalUrl,
+      message: "Api Not Found",
+    },
+  });
+  next();
 });
 
 export default app;
