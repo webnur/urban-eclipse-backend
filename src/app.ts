481976bec33786ad87f654d61d7ff3b5ecf0f05b
webnur <mdnuruli291@gmail.com>
2024-04-22 11:59:05 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+// const mongoose = require('mongoose');
+import mongoose from "mongoose";
+import app from "./app";
+const port = 3002;
+import config from "./config/index";
+import { Server } from "http";
+
+process.on("uncaughtException", (error) => {
+  console.error(error);
+  process.exit(1);
+});
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Database connected Successfully!");
+    server = app.listen(config.port, () => {
+      console.log(`Server is Running on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log("Failed to connected database", err);
+  }
+
+  process.on("unhandledRejection", (error) => {
+    if (server) {
+      server.close(() => {
+        console.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+main();
